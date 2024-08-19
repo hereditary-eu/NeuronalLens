2f20454d5460d576187740e254b560f4f3c65e90
MrP01 <peter@waldert.at>
2024-08-19 12:49:18 +0200

    integrate positions through the nanostore, highlight selector as a sphere

--- src/components/store.js
+++ src/components/store.js
@@ -1,3 +1,17 @@
 import { atom } from "nanostores";
+import * as d3 from "d3";
+
+export const positions = atom(
+  (await d3.dsv(" ", "/rank_0_positions.txt")).map((d) => {
+    return {
+      local_id: +d.local_id,
+      x: +d.x,
+      y: +d.y,
+      z: +d.z,
+      area: d.area,
+      type: d.type,
+    };
+  })
+);
 
 export const selector = atom({ local_id: 1 });
